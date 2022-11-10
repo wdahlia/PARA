e128636debad2b5689f68dbde8cf79c81955e157
star2871 <star2871@gmail.com>
2022-11-10 17:45:41 +0900

    song 카테고리 모델 추가및 프로덕트 모델 편집

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("review_create/", views.review_create, name="review_create"),
     path("<int:review_pk>/review_detail/", views.review_detail, name="review_detail"),
     path("<int:review_pk>/review_delete/", views.review_delete, name="review_delete"),
-    path("/<int:review_pk>/review_update/", views.review_update, name="review_update"),
+    path("<int:review_pk>/review_update/", views.review_update, name="review_update"),
 ]
