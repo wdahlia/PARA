80f602bd8d4c8195f2bd6dbbf2c54de0e69e8078
star2871 <star2871@gmail.com>
2022-11-10 14:14:23 +0900

    products 기본 설정 완료

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("accounts/", include("accounts.urls")),
     # path("chat/", include("chat.urls")),
-    # path("products/", include("products.urls")),
+    path("products/", include("products.urls")),
     # path("reviews/", include("reviews.urls")),
     path("admin/", admin.site.urls),
 ]
