e03d41024d54c180f79005428cccf8dc8d8055bc
dahlia <wlstnrdahlia@gmail.com>
2022-11-15 13:24:55 +0900

    add : products/index.html hover css 효과 추가
    - base.html 배경색 수정

--- accounts/views.py
+++ accounts/views.py
@@ -157,7 +157,7 @@ def logout(request):
 @login_required
 def update(request):
     if request.method == "POST":
-        form = CustomUserChangeForm(request.POST, instance=request.user)
+        form = CustomUserChangeForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
             return redirect("accounts:detail", request.user.pk)
