4bbd9a7e1f6a9cb2f9cb9f6266ab7e5a701bdaf0
taehopark <qkrxogh7@gmail.com>
2022-11-20 18:18:57 +0900

    test

--- config/management/commands/mysuperuser.py
+++ config/management/commands/mysuperuser.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        if not User.objects.filter(username="para").exists():
-            User.objects.create_superuser("para", "", "1234")
+# class Command(BaseCommand):
+#     def handle(self, *args, **options):
+#         if not User.objects.filter(username="para").exists():
+#             User.objects.create_superuser("para", "", "1234")
